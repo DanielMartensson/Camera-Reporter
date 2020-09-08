f0c43d499eac33b7afbcf72075a9373cca5008db
danielmartensson <daniel.martensson100@outlook.com>
2020-09-08 14:56:34 +0200

    Nya filer

--- src/main/java/se/martenssonborg/yolo/TrainModel.java
+++ src/main/java/se/martenssonborg/yolo/TrainModel.java
@@ -46,6 +46,7 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.util.ModelSerializer;
 import org.deeplearning4j.zoo.model.TinyYOLO;
+import org.deeplearning4j.zoo.model.YOLO2;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
@@ -63,188 +64,188 @@ import static org.bytedeco.opencv.global.opencv_core.RGB;
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
 
 /**
- * Example transfer learning from a Tiny YOLO model pretrained on ImageNet and Pascal VOC
- * to perform object detection with bounding boxes on The Street View House Numbers (SVHN) Dataset.
+ * Example transfer learning from a Tiny YOLO model pretrained on ImageNet and
+ * Pascal VOC to perform object detection with bounding boxes on The Street View
+ * House Numbers (SVHN) Dataset.
  * <p>
  * References: <br>
  * - YOLO: Real-Time Object Detection: https://pjreddie.com/darknet/yolo/ <br>
- * - The Street View House Numbers (SVHN) Dataset: http://ufldl.stanford.edu/housenumbers/ <br>
+ * - The Street View House Numbers (SVHN) Dataset:
+ * http://ufldl.stanford.edu/housenumbers/ <br>
  * <p>
- * Please note, cuDNN should be used to obtain reasonable performance: https://deeplearning4j.konduit.ai/config/backends/config-cudnn#using-deeplearning-4-j-with-cudnn
+ * Please note, cuDNN should be used to obtain reasonable performance:
+ * https://deeplearning4j.konduit.ai/config/backends/config-cudnn#using-deeplearning-4-j-with-cudnn
  *
  * @author saudet
  */
 public class TrainModel {
-    private static final Logger log = LoggerFactory.getLogger(TrainModel.class);
+	private static final Logger log = LoggerFactory.getLogger(TrainModel.class);
 
-    // Enable different colour bounding box for different classes
-    public static final Scalar RED = RGB(255.0, 0, 0);
-    public static final Scalar GREEN = RGB(0, 255.0, 0);
-    public static final Scalar BLUE = RGB(0, 0, 255.0);
-    public static final Scalar YELLOW = RGB(255.0, 255.0, 0);
-    public static final Scalar CYAN = RGB(0, 255.0, 255.0);
-    public static final Scalar MAGENTA = RGB(255.0, 0.0, 255.0);
-    public static final Scalar ORANGE = RGB(255.0, 128.0, 0);
-    public static final Scalar PINK = RGB(255.0, 192.0, 203.0);
-    public static final Scalar LIGHTBLUE = RGB(153.0, 204.0, 255.0);
-    public static final Scalar VIOLET = RGB(238.0, 130.0, 238.0);
+	// Enable different colour bounding box for different classes
+	public static final Scalar RED = RGB(255.0, 0, 0);
+	public static final Scalar GREEN = RGB(0, 255.0, 0);
+	public static final Scalar BLUE = RGB(0, 0, 255.0);
+	public static final Scalar YELLOW = RGB(255.0, 255.0, 0);
+	public static final Scalar CYAN = RGB(0, 255.0, 255.0);
+	public static final Scalar MAGENTA = RGB(255.0, 0.0, 255.0);
+	public static final Scalar ORANGE = RGB(255.0, 128.0, 0);
+	public static final Scalar PINK = RGB(255.0, 192.0, 203.0);
+	public static final Scalar LIGHTBLUE = RGB(153.0, 204.0, 255.0);
+	public static final Scalar VIOLET = RGB(238.0, 130.0, 238.0);
 
-    public static void main(String[] args) throws java.lang.Exception {
+	public static void main(String[] args) throws java.lang.Exception {
 
-        // parameters matching the pretrained TinyYOLO model
-        int width = 416;
-        int height = 416;
-        int nChannels = 3;
-        int gridWidth = 13;
-        int gridHeight = 13;
+		// parameters matching the pretrained TinyYOLO model
+		int width = 416;
+		int height = 416;
+		int nChannels = 3;
+		int gridWidth = 13;
+		int gridHeight = 13;
 
-        // number classes (digits) for the SVHN datasets
-        int nClasses = 10;
+		// number classes (digits) for the SVHN datasets
+		int nClasses = 10;
 
-        // parameters for the Yolo2OutputLayer
-        int nBoxes = 5;
-        double lambdaNoObj = 0.5;
-        double lambdaCoord = 1.0;
-        double[][] priorBoxes = {{2, 5}, {2.5, 6}, {3, 7}, {3.5, 8}, {4, 9}};
-        double detectionThreshold = 0.5;
+		// parameters for the Yolo2OutputLayer
+		int nBoxes = 5;
+		double lambdaNoObj = 0.5;
+		double lambdaCoord = 1.0;
+		double[][] priorBoxes = { { 2, 5 }, { 2.5, 6 }, { 3, 7 }, { 3.5, 8 }, { 4, 9 } };
+		double detectionThreshold = 0.5;
 
-        // parameters for the training phase
-        int batchSize = 10;
-        int nEpochs = 20;
-        double learningRate = 1e-4;
+		// parameters for the training phase
+		int batchSize = 10;
+		int nEpochs = 20;
+		double learningRate = 1e-4;
 
-        int seed = 123;
-        Random rng = new Random(seed);
+		int seed = 123;
+		Random rng = new Random(seed);
 
-        SvhnDataFetcher fetcher = new SvhnDataFetcher();
-        File trainDir = fetcher.getDataSetPath(DataSetType.TRAIN);
-        File testDir = fetcher.getDataSetPath(DataSetType.TEST);
+		SvhnDataFetcher fetcher = new SvhnDataFetcher();
+		File trainDir = fetcher.getDataSetPath(DataSetType.TRAIN);
+		File testDir = fetcher.getDataSetPath(DataSetType.TEST);
 
+		log.info("Load data...");
 
-        log.info("Load data...");
+		FileSplit trainData = new FileSplit(trainDir, NativeImageLoader.ALLOWED_FORMATS, rng);
+		FileSplit testData = new FileSplit(testDir, NativeImageLoader.ALLOWED_FORMATS, rng);
 
-        FileSplit trainData = new FileSplit(trainDir, NativeImageLoader.ALLOWED_FORMATS, rng);
-        FileSplit testData = new FileSplit(testDir, NativeImageLoader.ALLOWED_FORMATS, rng);
+		ObjectDetectionRecordReader recordReaderTrain = new ObjectDetectionRecordReader(height, width, nChannels, gridHeight, gridWidth, new SvhnLabelProvider(trainDir));
+		recordReaderTrain.initialize(trainData);
 
-        ObjectDetectionRecordReader recordReaderTrain = new ObjectDetectionRecordReader(height, width, nChannels,
-                        gridHeight, gridWidth, new SvhnLabelProvider(trainDir));
-        recordReaderTrain.initialize(trainData);
+		ObjectDetectionRecordReader recordReaderTest = new ObjectDetectionRecordReader(height, width, nChannels, gridHeight, gridWidth, new SvhnLabelProvider(testDir));
+		recordReaderTest.initialize(testData);
 
-        ObjectDetectionRecordReader recordReaderTest = new ObjectDetectionRecordReader(height, width, nChannels,
-                        gridHeight, gridWidth, new SvhnLabelProvider(testDir));
-        recordReaderTest.initialize(testData);
+		// ObjectDetectionRecordReader performs regression, so we need to specify it
+		// here
+		RecordReaderDataSetIterator train = new RecordReaderDataSetIterator(recordReaderTrain, batchSize, 1, 1, true);
+		train.setPreProcessor(new ImagePreProcessingScaler(0, 1));
 
-        // ObjectDetectionRecordReader performs regression, so we need to specify it here
-        RecordReaderDataSetIterator train = new RecordReaderDataSetIterator(recordReaderTrain, batchSize, 1, 1, true);
-        train.setPreProcessor(new ImagePreProcessingScaler(0, 1));
+		RecordReaderDataSetIterator test = new RecordReaderDataSetIterator(recordReaderTest, 1, 1, 1, true);
+		test.setPreProcessor(new ImagePreProcessingScaler(0, 1));
 
-        RecordReaderDataSetIterator test = new RecordReaderDataSetIterator(recordReaderTest, 1, 1, 1, true);
-        test.setPreProcessor(new ImagePreProcessingScaler(0, 1));
+		ComputationGraph model;
+		String modelFilename = "model.zip";
 
+		if (new File(modelFilename).exists()) {
+			log.info("Load model...");
 
-        ComputationGraph model;
-        String modelFilename = "model.zip";
+			model = ComputationGraph.load(new File(modelFilename), true);
+		} else {
+			log.info("Build model...");
 
-        if (new File(modelFilename).exists()) {
-            log.info("Load model...");
+			ComputationGraph pretrained = (ComputationGraph) YOLO2.builder().build().initPretrained();
+			INDArray priors = Nd4j.create(priorBoxes);
 
-            model = ComputationGraph.load(new File(modelFilename), true);
-        } else {
-            log.info("Build model...");
+			FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder()
+					.seed(seed)
+					.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+					.gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+					.gradientNormalizationThreshold(1.0)
+					.updater(new Adam.Builder().learningRate(learningRate).build())
+					// .updater(new
+					// Nesterovs.Builder().learningRate(learningRate).momentum(lrMomentum).build())
+					.l2(0.00001)
+					.activation(Activation.IDENTITY)
+					.trainingWorkspaceMode(WorkspaceMode.ENABLED)
+					.inferenceWorkspaceMode(WorkspaceMode.ENABLED)
+					.build();
 
-            ComputationGraph pretrained = (ComputationGraph)TinyYOLO.builder().build().initPretrained();
-            INDArray priors = Nd4j.create(priorBoxes);
+			model = new TransferLearning.GraphBuilder(pretrained)
+					.fineTuneConfiguration(fineTuneConf)
+					.removeVertexKeepConnections("conv2d_9")
+					.removeVertexKeepConnections("outputs")
+					.addLayer("convolution2d_9",
+							new ConvolutionLayer.Builder(1, 1)
+									.nIn(1024)
+									.nOut(nBoxes * (5 + nClasses))
+									.stride(1, 1)
+									.convolutionMode(ConvolutionMode.Same)
+									.weightInit(WeightInit.XAVIER)
+									.activation(Activation.IDENTITY)
+									.build(),
+							"leaky_re_lu_8")
+					.addLayer("outputs",
+							new Yolo2OutputLayer.Builder()
+									.lambdaNoObj(lambdaNoObj)
+									.lambdaCoord(lambdaCoord)
+									.boundingBoxPriors(priors)
+									.build(),
+							"convolution2d_9")
+					.setOutputs("outputs")
+					.build();
+			System.out.println(model.summary(InputType.convolutional(height, width, nChannels)));
 
-            FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder()
-                    .seed(seed)
-                    .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                    .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
-                    .gradientNormalizationThreshold(1.0)
-                    .updater(new Adam.Builder().learningRate(learningRate).build())
-                    //.updater(new Nesterovs.Builder().learningRate(learningRate).momentum(lrMomentum).build())
-                    .l2(0.00001)
-                    .activation(Activation.IDENTITY)
-                    .trainingWorkspaceMode(WorkspaceMode.ENABLED)
-                    .inferenceWorkspaceMode(WorkspaceMode.ENABLED)
-                    .build();
+			log.info("Train model...");
 
-            model = new TransferLearning.GraphBuilder(pretrained)
-                    .fineTuneConfiguration(fineTuneConf)
-                    .removeVertexKeepConnections("conv2d_9")
-                    .removeVertexKeepConnections("outputs")
-                    .addLayer("convolution2d_9",
-                            new ConvolutionLayer.Builder(1,1)
-                                    .nIn(1024)
-                                    .nOut(nBoxes * (5 + nClasses))
-                                    .stride(1,1)
-                                    .convolutionMode(ConvolutionMode.Same)
-                                    .weightInit(WeightInit.XAVIER)
-                                    .activation(Activation.IDENTITY)
-                                    .build(),
-                            "leaky_re_lu_8")
-                    .addLayer("outputs",
-                            new Yolo2OutputLayer.Builder()
-                                    .lambdaNoObj(lambdaNoObj)
-                                    .lambdaCoord(lambdaCoord)
-                                    .boundingBoxPriors(priors)
-                                    .build(),
-                            "convolution2d_9")
-                    .setOutputs("outputs")
-                    .build();
-            System.out.println(model.summary(InputType.convolutional(height, width, nChannels)));
+			model.setListeners(new ScoreIterationListener(1));
+			model.fit(train, nEpochs);
 
+			log.info("Save model...");
+			ModelSerializer.writeModel(model, modelFilename, true);
+		}
 
-            log.info("Train model...");
+		// visualize results on the test set
+		NativeImageLoader imageLoader = new NativeImageLoader();
+		CanvasFrame frame = new CanvasFrame("HouseNumberDetection");
+		OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+		org.deeplearning4j.nn.layers.objdetect.Yolo2OutputLayer yout = (org.deeplearning4j.nn.layers.objdetect.Yolo2OutputLayer) model.getOutputLayer(0);
+		List<String> labels = train.getLabels();
+		test.setCollectMetaData(true);
+		Scalar[] colormap = { RED, BLUE, GREEN, CYAN, YELLOW, MAGENTA, ORANGE, PINK, LIGHTBLUE, VIOLET };
 
-            model.setListeners(new ScoreIterationListener(1));
-            model.fit(train, nEpochs);
+		while (test.hasNext() && frame.isVisible()) {
+			org.nd4j.linalg.dataset.DataSet ds = test.next();
+			RecordMetaDataImageURI metadata = (RecordMetaDataImageURI) ds.getExampleMetaData().get(0);
+			INDArray features = ds.getFeatures();
+			INDArray results = model.outputSingle(features);
+			List<DetectedObject> objs = yout.getPredictedObjects(results, detectionThreshold);
+			File file = new File(metadata.getURI());
+			log.info(file.getName() + ": " + objs);
 
-            log.info("Save model...");
-            ModelSerializer.writeModel(model, modelFilename, true);
-        }
+			Mat mat = imageLoader.asMat(features);
+			Mat convertedMat = new Mat();
+			mat.convertTo(convertedMat, CV_8U, 255, 0);
+			int w = metadata.getOrigW() * 2;
+			int h = metadata.getOrigH() * 2;
+			Mat image = new Mat();
+			resize(convertedMat, image, new Size(w, h));
+			for (DetectedObject obj : objs) {
+				double[] xy1 = obj.getTopLeftXY();
+				double[] xy2 = obj.getBottomRightXY();
+				String label = labels.get(obj.getPredictedClass());
+				int x1 = (int) Math.round(w * xy1[0] / gridWidth);
+				int y1 = (int) Math.round(h * xy1[1] / gridHeight);
+				int x2 = (int) Math.round(w * xy2[0] / gridWidth);
+				int y2 = (int) Math.round(h * xy2[1] / gridHeight);
+				rectangle(image, new Point(x1, y1), new Point(x2, y2), colormap[obj.getPredictedClass()]);
+				putText(image, label, new Point(x1 + 2, y2 - 2), FONT_HERSHEY_DUPLEX, 1, colormap[obj.getPredictedClass()]);
 
-        // visualize results on the test set
-        NativeImageLoader imageLoader = new NativeImageLoader();
-        CanvasFrame frame = new CanvasFrame("HouseNumberDetection");
-        OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
-        org.deeplearning4j.nn.layers.objdetect.Yolo2OutputLayer yout = (org.deeplearning4j.nn.layers.objdetect.Yolo2OutputLayer)model.getOutputLayer(0);
-        List<String> labels = train.getLabels();
-        test.setCollectMetaData(true);
-        Scalar[] colormap = {RED,BLUE,GREEN,CYAN,YELLOW,MAGENTA,ORANGE,PINK,LIGHTBLUE,VIOLET};
-
-        while (test.hasNext() && frame.isVisible()) {
-            org.nd4j.linalg.dataset.DataSet ds = test.next();
-            RecordMetaDataImageURI metadata = (RecordMetaDataImageURI)ds.getExampleMetaData().get(0);
-            INDArray features = ds.getFeatures();
-            INDArray results = model.outputSingle(features);
-            List<DetectedObject> objs = yout.getPredictedObjects(results, detectionThreshold);
-            File file = new File(metadata.getURI());
-            log.info(file.getName() + ": " + objs);
-
-            Mat mat = imageLoader.asMat(features);
-            Mat convertedMat = new Mat();
-            mat.convertTo(convertedMat, CV_8U, 255, 0);
-            int w = metadata.getOrigW() * 2;
-            int h = metadata.getOrigH() * 2;
-            Mat image = new Mat();
-            resize(convertedMat, image, new Size(w, h));
-            for (DetectedObject obj : objs) {
-                double[] xy1 = obj.getTopLeftXY();
-                double[] xy2 = obj.getBottomRightXY();
-                String label = labels.get(obj.getPredictedClass());
-                int x1 = (int) Math.round(w * xy1[0] / gridWidth);
-                int y1 = (int) Math.round(h * xy1[1] / gridHeight);
-                int x2 = (int) Math.round(w * xy2[0] / gridWidth);
-                int y2 = (int) Math.round(h * xy2[1] / gridHeight);
-                rectangle(image, new Point(x1, y1), new Point(x2, y2), colormap[obj.getPredictedClass()]);
-                putText(image, label, new Point(x1 + 2, y2 - 2), FONT_HERSHEY_DUPLEX, 1, colormap[obj.getPredictedClass()]);
-
-            }
-            frame.setTitle(new File(metadata.getURI()).getName() + " - HouseNumberDetection");
-            frame.setCanvasSize(w, h);
-            frame.showImage(converter.convert(image));
-            frame.waitKey();
-        }
-        frame.dispose();
-    }
+			}
+			frame.setTitle(new File(metadata.getURI()).getName() + " - HouseNumberDetection");
+			frame.setCanvasSize(w, h);
+			frame.showImage(converter.convert(image));
+			frame.waitKey();
+		}
+		frame.dispose();
+	}
 }
